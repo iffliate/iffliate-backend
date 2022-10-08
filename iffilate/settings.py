a331ecc47899bca18f52d879e0f5d2bd7ee1d24f
markothedev <markothedevmail@gmail.com>
2022-10-08 02:34:04 +0100

    added a new enviroment var

--- iffilate/settings.py
+++ iffilate/settings.py
@@ -96,14 +96,15 @@ WSGI_APPLICATION = 'iffilate.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {}
-if DEBUG:
+use_postgre = config('use_postgre',cast=bool)
+if use_postgre:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)  
+    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+else:
      DATABASES['default']={
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)  
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 # Password validation
