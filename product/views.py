0149b312c135f08ca46b5910f56a5f10d597e302
markothedev <markothedevmail@gmail.com>
2022-10-22 18:07:45 +0100

    updated get_allPaystack endpoint for shop

--- product/views.py
+++ product/views.py
@@ -124,7 +124,7 @@ class ShopOrderManagement(mixins.ListModelMixin,mixins.UpdateModelMixin,mixins.R
     @action(detail=True,methods=['get'])
     def get_all_paystack_keys(self,request,pk=None):
         'we getting all the keys that will be represented as payment id for the shop'
-        paystack_keys = OrderHistory.objects.filter(shop__user=pk).values('paystack').distinct()
+        paystack_keys = OrderHistory.objects.filter(shop=pk).values('paystack').distinct()
         return Success_response(msg="Success",data=paystack_keys,status_code =status.HTTP_200_OK)
 
 
